9124d71c26fe74c09a7b37afeb5d3ef567377153
teskew <teskinder1@gmail.com>
2020-12-01 18:46:27 -0600

    commit tree 7c51bbe7807a6ee8e396826c9c1f4c16df1c6052

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,32 @@
 # Add your code here
+require 'pry'
+class Dog
+  attr_reader :name
+  @@all = []
+def initialize(name)
+    @name = name
+    save
+end
+  
+ def self.all
+    @@all
+   
+ end
+ def save
+  @@all << self
+  end
+def self.clear_all
+  @@all =[]
+end
+def self.print_all
+  @@all.each do |dog|
+   puts dog.name
+  end
+
+  end
+ def name
+  @name
+ end
+
+end
+
